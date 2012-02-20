b885744f65a0ce5b41041e5bdc25b6232b27c4e3
Jeff Beeman <jeff@jeffbeeman.com>
2012-02-20 08:51:40 -0700

    Use submodule for js2-mode

--- init.el
+++ init.el
@@ -133,6 +133,7 @@
 
 
 ; JS2 (JavaScript) mode
+(add-to-list 'load-path "~/.emacs.d/vendor/js2-mode")
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
--- vendor/js2-mode.el
+++
@@ -1,1 +0,0 @@
-/Users/jbeeman/Projects/github/js2-mode/js2-mode.el
